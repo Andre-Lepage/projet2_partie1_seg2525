8e6f57c5bc7749045cb80914f249c6b8606c9a35
Andre-Lepage <andrelepage15@gmail.com>
2023-07-02 23:01:35 -0400

    trying to get deploy working

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   return (
 
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/Categories' element={<Categories/>} />
